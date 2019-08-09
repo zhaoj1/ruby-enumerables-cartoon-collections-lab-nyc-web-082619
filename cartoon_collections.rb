aa2efcf447c34048006185181e8688b6c290777f
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-09 18:37:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,19 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.each_with_index{ |name,index|
+    puts "/#{index+1}. *#{name}/"
+  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(elements)
+  elements.collect{ |elements| 
+    elements.capitalize + "!"
+  }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+  words.all?{
+    
+  }
 end
 
 def find_the_cheese# code an argument here
