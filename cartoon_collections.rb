0dc3b6c95832cc9454097d4137b48d54be6f15a9
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-09 18:43:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def long_planeteer_calls(words)
   words.any? { |x| x.size > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese()
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
