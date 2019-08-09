f1537658d4888b66f5b56a2294b3ee419961d2a7
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-09 18:42:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(elements)
 end
 
 def long_planeteer_calls(words)
-  words.all? { |x| x.size > 4 }
+  words.any? { |x| x.size > 4 }
 end
 
 def find_the_cheese# code an argument here
